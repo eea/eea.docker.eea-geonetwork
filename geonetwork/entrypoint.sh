1e7223f46329914850474a958581d14c049e903d
Mauro Michielon - EEA 06Oct2015 <michimau@dev-mil-24.pecs.eea>
2016-09-20 20:36:51 +0200

    fix remove of undesired default webapps

--- geonetwork/entrypoint.sh
+++ geonetwork/entrypoint.sh
@@ -6,7 +6,7 @@ sed "s#PASSWORD#$PASSWORD#g" -i /jdbc.properties
 sed "s#DATABASE#$DATABASE#g" -i /jdbc.properties
 sed "s#HOST#$HOST#g" -i /jdbc.properties
 
-rm /usr/local/tomcat/webapps/*
+rm -rf /usr/local/tomcat/webapps/*
 mv target/$CATALOGUE.war /usr/local/tomcat/webapps
 
-#catalina.sh run
+catalina.sh run
