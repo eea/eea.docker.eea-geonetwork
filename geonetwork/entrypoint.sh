5375a1fe8c78980588829ed7d8ec4733b4a49a5f
Mauro Michielon <mauro.michielon@eea.europa.eu>
2016-10-13 15:08:59 +0200

    java opts update and persistent storage

--- geonetwork/entrypoint.sh
+++ geonetwork/entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -e
 
+mkdir -p /catalogue-data/htmlcache
+mkdir -p /editor-catalogue-data/htmlcache
+mkdir -p /internal-catalogue-data/htmlcache
+mkdir -p /map-catalogue-data/htmlcache
+
+mkdir -p /catalogue-data/lucene
+mkdir -p /editor-catalogue-data/lucene
+mkdir -p /internal-catalogue-data/lucene
+mkdir -p /map-catalogue-data/lucene
+
 sed "s#USERNAME#$USERNAME#g" -i /jdbc.properties
 sed "s#PASSWORD#$PASSWORD#g" -i /jdbc.properties
 sed "s#DATABASE#$DATABASE#g" -i /jdbc.properties
 sed "s#HOST#$HOST#g" -i /jdbc.properties
+
+cp /jdbc.properties /catalogue-data/jdbc.properties
+cp /jdbc.properties /editor-catalogue-data/jdbc.properties
+cp /jdbc.properties /internal-catalogue-data/jdbc.properties
+cp /jdbc.properties /map-catalogue-data/jdbc.properties
 
 rm -rf /usr/local/tomcat/webapps/*
 mv target/$CATALOGUE.war /usr/local/tomcat/webapps
