6f45bb3ca4801f94c45ee832a047cbcf534a1ad2
Mauro Michielon <mauro.michielon@eea.europa.eu>
2016-10-17 13:06:54 +0200

    various modifications

--- geonetwork/entrypoint.sh
+++ geonetwork/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+#wget $URL -O /eea-catalogues.tar.gz
+#
+#tar -xvzf /eea-catalogues.tar.gz -C /
+
 mkdir -p /catalogue-data/htmlcache
 mkdir -p /editor-catalogue-data/htmlcache
 mkdir -p /internal-catalogue-data/htmlcache
@@ -21,8 +25,8 @@ cp /jdbc.properties /editor-catalogue-data/jdbc.properties
 cp /jdbc.properties /internal-catalogue-data/jdbc.properties
 cp /jdbc.properties /map-catalogue-data/jdbc.properties
 
-rm -rf /usr/local/tomcat/webapps/*
-mv target/$CATALOGUE.war /usr/local/tomcat/webapps
+#rm -rf /usr/local/tomcat/webapps/*
+#mv /target/$CATALOGUE.war /usr/local/tomcat/webapps
 
 exec catalina.sh run "$@"
 
