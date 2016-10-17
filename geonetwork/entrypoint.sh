a8209534cf4eb6d6008b0af717ab2bd61d940c2a
Mauro Michielon - EEA 06Oct2015 <michimau@galliwasp.eea.europa.eu>
2016-10-17 15:01:12 +0200

    several changes

--- geonetwork/entrypoint.sh
+++ geonetwork/entrypoint.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 set -e
 
-#wget $URL -O /eea-catalogues.tar.gz
-#
-#tar -xvzf /eea-catalogues.tar.gz -C /
+mkdir -p /$CATALOGUE-data/htmlcache
 
-mkdir -p /catalogue-data/htmlcache
-mkdir -p /editor-catalogue-data/htmlcache
-mkdir -p /internal-catalogue-data/htmlcache
-mkdir -p /map-catalogue-data/htmlcache
-
-mkdir -p /catalogue-data/lucene
-mkdir -p /editor-catalogue-data/lucene
-mkdir -p /internal-catalogue-data/lucene
-mkdir -p /map-catalogue-data/lucene
+mkdir -p /$CATALOGUE-data/lucene
 
 sed "s#USERNAME#$USERNAME#g" -i /jdbc.properties
 sed "s#PASSWORD#$PASSWORD#g" -i /jdbc.properties
 sed "s#DATABASE#$DATABASE#g" -i /jdbc.properties
 sed "s#HOST#$HOST#g" -i /jdbc.properties
 
-cp /jdbc.properties /catalogue-data/jdbc.properties
-cp /jdbc.properties /editor-catalogue-data/jdbc.properties
-cp /jdbc.properties /internal-catalogue-data/jdbc.properties
-cp /jdbc.properties /map-catalogue-data/jdbc.properties
+cp /jdbc.properties /$CATALOGUE-data/jdbc.properties
 
-#rm -rf /usr/local/tomcat/webapps/*
-#mv /target/$CATALOGUE.war /usr/local/tomcat/webapps
+cp /target/$CATALOGUE.war /usr/local/tomcat/webapps 
+echo $CATALOGUE.war artifact copied in webapps
 
 exec catalina.sh run "$@"
 
