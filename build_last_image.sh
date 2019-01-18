ed79879307172fa0e73665a42050510665a1648b
Mauro Michielon <mauro.michielon@eea.europa.eu>
2019-01-18 15:53:17 +0100

    sorting by date

--- build_last_image.sh
+++ build_last_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #exec > /dev/null 2>&1
-id=$(curl -s https://files.titellus.net/geonetwork/eea/ | grep eea-catalogues | tail -1 | cut -c 135-174  )
+id=$(curl -s https://files.titellus.net/geonetwork/eea/?C=M | grep eea-catalogues | tail -1 | cut -c 135-174  )
 
 echo building for $id
 docker build -t eeacms/eea-geonetwork:$id --build-arg id=$id ./geonetwork/
