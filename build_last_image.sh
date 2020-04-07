97f82a4eade2ff94b9096045faf5481a0b1fb1aa
Mauro Michielon <mauro.michielon@eea.europa.eu>
2020-04-07 15:15:33 +0200

    consolidating

--- build_last_image.sh
+++ build_last_image.sh
@@ -2,8 +2,8 @@
 
 #exec > /dev/null 2>&1
 id=$(curl -s https://files.titellus.net/geonetwork/eea/?C=M | grep eea-catalogues | tail -1 | cut -c 135-174  )
-
+#id=4060a422fada8c66b41105b6789f8bf6285641e4
 echo building for $id
-docker build -t eeacms/eea-geonetwork:$id --build-arg id=$id ./geonetwork/
+docker build --no-cache -t eeacms/eea-geonetwork:$id --build-arg id=$id ./geonetwork/
 
 echo build ready for $id
