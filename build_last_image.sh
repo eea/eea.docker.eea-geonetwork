21ab4b91b0cf2f0bdaed30c5d887a4f073ba0e4a
Mauro Michielon <mauro.michielon@eea.europa.eu>
2022-08-25 13:45:35 +0200

    last commit before leaving

--- build_last_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#exec > /dev/null 2>&1
-id=$(curl -s https://files.titellus.net/geonetwork/eea/?C=M | grep eea-catalogues | tail -1 | cut -c 135-174  )
-#id=4060a422fada8c66b41105b6789f8bf6285641e4
-echo building for $id
-docker build --no-cache -t eeacms/eea-geonetwork:$id --build-arg id=$id ./geonetwork/
-
-echo build ready for $id
